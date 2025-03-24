d9acd91217c1f3d1b67055f82aef9c0632dbdda8
Enotiha <artemkualmazov@yandex.ru>
2025-03-24 18:16:31 +0700

    #22 GameField/Button

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -2,15 +2,14 @@ import logoSrc from "./logo.svg";
 import Image from "next/image";
 import { Profile } from "../profile";
 import { ArrowDownIcon } from "./icons/arrow-down-icon";
+import { UiButton } from "../uikit/ui-button";
 
 export function Header() {
   return (
     <header className="flex h-24 items-center px-8 bg-white shadow-lg">
       <Image src={logoSrc} alt="logo" />
       <div className="w-px h-8 bg-slate-200 mx-6"></div>
-      <button className="w-44 bg-teal-600 hover:bg-teal-500 transition-colors text-white rounded-lg px-5 py-2 text-2xl leading-tight">
-        Играть
-      </button>
+      <UiButton variant="primary" size="lg" children="Играть" />
       <button className="ml-auto flex items-center gap-2 text-teal-600">
         <Profile />
         <ArrowDownIcon />

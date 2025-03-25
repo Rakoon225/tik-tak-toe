afa957d128afceb23704f7b62050bd9bbb8cb689
Enotiha <artemkualmazov@yandex.ru>
2025-03-25 13:50:49 +0700

    #23/useState-and-gameGrid-logic

---
+++ components/game/use-game-state.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { GAME_SYMBOLS, MOVE_ORDER } from "./constants";
+
+
+function getNextMove(currentMove) {
+  const nextMoveIndex = MOVE_ORDER.indexOf(currentMove) + 1;
+  return MOVE_ORDER[nextMoveIndex % MOVE_ORDER.length];
+}
+
+export function useGamestate(){
+    const [{ cells, currentMove }, setGameState] = useState(() => ({
+        cells: new Array(19 * 19).fill(null),
+        currentMove: GAME_SYMBOLS.CROSS,
+      }))
+
+      const nextMove = getNextMove(currentMove);
+      const handleCellClick = (index) => {
+        setGameState((lastGamestate) => {
+
+          if(lastGamestate.cells[index]){
+            return lastGamestate
+          }
+
+          return{
+            ...lastGamestate,
+            currentMove: getNextMove(lastGamestate.currentMove),
+            cells: lastGamestate.cells.map((cell, i) =>
+            i == index ? lastGamestate.currentMove : cell,
+            ),
+          }
+        });
+      }
+
+      return{
+        cells, currentMove, nextMove, handleCellClick
+      }
+
+}
